47370cb4718816849c33fd922441b479bc420801
will jr <wjr46269@gmail.com>
2024-09-27 01:40:42 +0200

    changed the icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "./components/theme-provider";
 import SessionWrapper from "./components/SessionWrapper";
-import Navbar from "./components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
